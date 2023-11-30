7e43a7adaa449065efabefc4d05afeff862f0b80
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-30 16:10:53 +0530

    Updated DB schema name

--- BackEnd/util/database.js
+++ BackEnd/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'root', {
+const sequelize = new Sequelize('booking-appointment-app', 'root', 'root', {
     dialect: 'mysql',
     host: 'localhost'
 });
